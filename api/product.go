4ff2af02c6a84f32ece4ae21e2c5087245a446dc
Vivek T S <vivekts.dev@gmail.com>
2024-07-08 02:15:54 +0530

    additional validations

--- api/product.go
+++ api/product.go
@@ -5,6 +5,10 @@ import (
     "unicode"
 )
 
+var (
+    ProductCutOffDate = time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
+)
+
 type Product struct {
     ID           string    `json:"id"`
     Name         string    `json:"name"`
@@ -33,16 +37,29 @@ func (pc *ProductCreate) Validate() []ErrorDetail {
     nameError := ErrorDetail{
         Field: "name",
     }
-    if len(pc.Name) <= 0 || len(pc.Name) > 200 {
-        nameError.AppendMessage("name must have between 1 and 200 characters")
-    }
-    if len(pc.Name) > 0 {
+    if len(pc.Name) == 0 {
+        nameError.AppendMessage("must not be empty")
+    } else if len(pc.Name) > 0 {
         if !unicode.IsLetter(rune(pc.Name[0])) {
             nameError.AppendMessage("first letter must be an alphabet")
+        }
+        if len(pc.Name) > 200 {
+            nameError.AppendMessage("must not exceed 200 characters")
         }
     }
     if nameError.HasMessages() {
         errorDetail = append(errorDetail, nameError)
+    }
+    rDateError := ErrorDetail{
+        Field: "releasedDate",
+    }
+    if pc.ReleasedDate == nil {
+        rDateError.AppendMessage("must not be empty")
+    } else if time.Time(*pc.ReleasedDate).Before(ProductCutOffDate) {
+        rDateError.AppendMessage("must be on or after product cutoff date 2000-01-01")
+    }
+    if rDateError.HasMessages() {
+        errorDetail = append(errorDetail, rDateError)
     }
     return errorDetail
 }
